80cafe57d786d83c101eea11d2e0369598c7112e
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-03-04 20:18:32 +0100

    Turn the GET route into a simple redirect (no need to parse anything for those IIUC).

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,14 @@ use reqwest::{Client, Response};
 use serde_json::Value;
 use warp::filters::body;
 use warp::filters::path::{full, FullPath};
+use warp::http::Uri;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
   let target_host = "https://api.tzstats.com";
 
-  let get_route = warp::get()
-    .and(full())
-    .and(body::json())
-    .and_then(async move |full_route: FullPath, mut body: Value| {
-      hijack(&mut body);
-      Client::new()
-        .get(&format!("{}{}", target_host, full_route.as_str()))
-        .header("Host", "api.tzstats.com")
-        .json(&body)
-        .send()
-        .await
-        .map_err(|_| warp::reject::not_found())
-    })
-    .and_then(async move |resp: Response| {
-      resp
-        .json::<Value>()
-        .await
-        .map_err(|_| warp::reject::not_found())
-    })
-    .map(|j| warp::reply::json(&j));
+  let get_route = warp::get().map(move || warp::redirect::temporary(Uri::from_static(target_host)));
 
   let post_route = warp::post()
     .and(full())
